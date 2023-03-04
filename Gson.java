87fa4c6a09838be16448cdd33d76b23bbd266437
RockyDogs <116272967+RockyDogs@users.noreply.github.com>
2023-03-04 17:51:23 +0500

    Create Gson.java

---
+++ Gson.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Gson {
+
+    public String toJson(List<Map> result) {
+        return null;
+    }
+
+    public List<Map> fromJson(String text, Class<ArrayList> class1) {
+        return null;
+    }
+
+}
