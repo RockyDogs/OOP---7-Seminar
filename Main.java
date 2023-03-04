812f3d3a9311e193d1cf4c9e972e8d6170c7f4bd
RockyDogs <116272967+RockyDogs@users.noreply.github.com>
2023-03-04 18:09:58 +0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+import controllers.AdapterController;
+import controllers.IController;
+import loggers.ILogger;
+import loggers.Logger;
+import model.*;
+import views.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ILogger logger = new Logger("log.txt");
+        IFileOperation fileOperation = new FileOperation("notes.txt");
+        IRepository repository = new Repository(new NotesMapperJSON(), fileOperation);
+        IController controller = new AdapterController(repository);
+        IViewOperations viewOperations = new ViewOperations(controller);
+        IViewOperations viewOperationsWithLog = new ViewOperationWithLog(viewOperations, logger);
+        IViewNote view = new ViewNote(viewOperationsWithLog);
+        view.run();
+    }
+}
+
+
+
